182c731504e75a17beb1f83d037aa37d68a01645
Gui Mattos <guidemattosmarques@gmail.com>
2026-06-08 13:58:38 -0300

    Array 2D em C, printado com formatação teclado numérico

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// Testando se um numero eh par ou impar
+int main() {
+    
+    int matrix[3][3] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int rows = (int) sizeof(matrix) / sizeof(matrix[0]);
+    int cols = (int) sizeof(matrix[0]) / sizeof(matrix[0][0]);
+
+    for (int i = 0; i < rows; i++) {
+        
+        for (int j = 0; j < cols; j++) {
+
+            if (j == cols - 1) {
+                printf("%d", matrix[i][j]);
+            }
+            else
+            {
+                printf("%d | ", matrix[i][j]);
+            }
+        }
+
+        (i == rows - 1) ? printf("\n") : printf("\n----------\n");
+    }
+    
+    return 0;
+}
